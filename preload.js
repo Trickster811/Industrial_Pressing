75f1921325db4ec236a7fb0c123f1e8eb0096d3f
Awoulka <awoulkacim@gmail.com>
2023-10-17 10:41:19 +0100

    correction d'une erreur de merge entre la branche master et adb

--- preload.js
+++ preload.js
@@ -1,22 +1,6 @@
 
 // Connection to MySQL database via the package SEQUELIZE
 const { Sequelize, DataTypes, QueryTypes } = require("sequelize");
-
-const { Client } = require("./src/models/client.model/client.model");
-
-// Find all users
-async function select_user() {
-    // Create a new user
-    //const jane = await Client.create({ nomClient: "Jane", phoneClient: 677345578 });
-    //console.log("Jane's auto-generated ID:", jane.idClient);
-
-    const users = await Client.findAll();
-    //console.log(users.every(user => user instanceof User)); // true
-    console.log("All users:", JSON.stringify(users, null, 2));
-
-}
-
-select_user()
 const { contextBridge } = require("electron");
 const { Service } = require("./src/models/service.model/service.model");
 const { Client } = require("./src/models/client.model/client.model");

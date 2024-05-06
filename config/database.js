565ed2e498ce6f51f70675cf8c60bac74591540f
Trickster811 <nedaoukajoachim@gmail.com>
2024-05-06 22:28:22 +0200

    commit

--- config/database.js
+++ config/database.js
@@ -2,20 +2,20 @@
 const { Sequelize, DataTypes } = require("sequelize");
 
 // Enable connection with database
-// const sequelize = new Sequelize( {
-//   dialect: "sqlite",
-//   storage: './industrial_pressing.base.sqlite'
-// });
+const sequelize = new Sequelize( {
+  dialect: "sqlite",
+  storage: './industrial_pressing.base.sqlite'
+});
 
 // const sequelize = new Sequelize("industrial_pressing_base", "postgres", "leslie02", {
 //   host: "localhost",
 //   dialect: "postgres", 
 // });
 
-const sequelize = new Sequelize("industrial_pressing_base", "root", "", {
-    host: "localhost",
-    dialect: "mysql", 
-  });
+// const sequelize = new Sequelize("industrial_pressing_base", "root", "", {
+//     host: "localhost",
+//     dialect: "mysql", 
+//   });
 
 // Sync database
 sequelize
